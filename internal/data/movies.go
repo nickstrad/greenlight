69a323b11e55e39a8c2905d4a50a4cac5779fa62
Nick Strad <nickstrad.dev@gmail.com>
2022-12-31 10:40:49 -0600

    Basci crud functionality working with postgres

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,8 +1,11 @@
 package data
 
 import (
+	"database/sql"
+	"errors"
 	"time"
 
+	"github.com/lib/pq"
 	"github.com/nickstrad/greenlight/internal/validator"
 )
 
@@ -30,4 +33,94 @@ func ValidateMovie(v *validator.Validator, m *Movie) {
 	v.Check(len(m.Genres) >= 1, "genres", "must contain at least 1 genre")
 	v.Check(len(m.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	v.Check(validator.Unique(m.Genres), "genres", "must not contain duplicate values")
+}
+
+type MovieModel struct {
+	DB *sql.DB
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	query := `
+	insert into movies(title, year, runtime, genres)
+	values ($1, $2, $3, $4)
+	returning id, created_at, version`
+
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `
+	select id, created_at, title, year, runtime, genres, version
+	from movies
+	where id = $1`
+
+	var movie Movie
+
+	err := m.DB.QueryRow(query, id).Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		pq.Array(&movie.Genres),
+		&movie.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &movie, nil
+}
+
+func (m MovieModel) Update(movie *Movie) error {
+	query := `update movies
+	set title = $1, year = $2, runtime = $3, genres = $4,
+	version = version +1
+	where id = $5
+	returning version`
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+}
+
+func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `delete from movies where id = $1;`
+	res, err := m.DB.Exec(query, id)
+
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
+	return nil
 }
---
+++ migrations/000002_add_movies_check_constraints.down.sql
@@ -0,0 +1,3 @@
+alter table drop constraint if exists movies_runtime_check;
+alter table drop constraint if exists movies_year_check;
+alter table drop constraint if exists genres_length_check;
